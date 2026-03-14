da7ed0d5dcfb484c308bd9f5d9c6ee1049d51350
Nickolay Pruzhnyak <niklkoud20@gmail.com>
2026-03-15 00:16:28 +0200

    HW10

--- Homework5/9/script_exercise_9.sh
+++ Homework5/9/script_exercise_9.sh
@@ -1,7 +1,17 @@
+  GNU nano 7.2                                           script_exercise_9.sh
 #!/bin/bash
 
-read -p "Enter the file path? which need to copy - " PATH1
-read -p "Enter the path where need to paste the file - " PATH2
+NAME_FILE=$1
 
+#read -p "Enter the file name: " NAME_FILE
 
-cp $PATH1 $PATH2
+if [ ! -e "$NAME_FILE" ]
+then
+        echo "$NAME_FILE is not exists"
+elif [ ! -s "$NAME_FILE" ]
+then
+        echo "$NAME_FILE is empty"
+else
+        echo "Content the $NAME_FILE : "
+        cat "$NAME_FILE"
+fi
