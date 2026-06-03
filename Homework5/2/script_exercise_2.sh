7715f45226c30a82e7ccf100ba38a4ac60e5124c
niklper <niklkoud20@gmail.com>
2026-06-03 15:35:51 +0300

    Удаление лишних файлов

--- Homework5/2/script_exercise_2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#this script display greets
-
-read -p "Enter the name: " NAME
-
-echo "Hello $NAME"
--- Homework5/6/script_exercise_6.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-read -p "Enter the text: " TEXT
-
-echo "$TEXT" | awk '{for(i=NF;i>=1;i--) printf "%s ", $i; print ""}'
